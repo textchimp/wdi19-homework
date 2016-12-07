4a6ae5e7cac0938a719b5efa30562a1abd2bf305
Alexander Gaggero <alexandergaggero@Alexanders-MacBook-Pro.local>
2016-12-08 10:40:48 +1100

    AG wednesdays hw

---
+++ alexander_gaggero/week_01/wednesday/js/exercise6.js
@@ -0,0 +1,16 @@
+/*
+Write a function findLongestWord that takes an array of words and returns the length of the longest one.
+*/
+
+var words = ['President', 'Premier', 'Chief'];
+
+var range = 0;
+var longest;
+
+for(var i=0; i < words.length; i++){
+    if(words[i].length > lgth) {
+        var range = words[i].length;
+        longest = words[i];
+    }
+}
+return(longest);
