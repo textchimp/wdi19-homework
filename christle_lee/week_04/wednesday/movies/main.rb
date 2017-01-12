2dd98e5553109838c34110035b9ed34feda53efc
Christle Lee <christle.lee86@gmail.com>
2017-01-13 10:54:56 +1100

    sql

--- christle_lee/week_04/wednesday/movies/main.rb
+++ christle_lee/week_04/wednesday/movies/main.rb
@@ -7,12 +7,19 @@ get '/' do
   erb :home #file in views folder
 end
 
-# get '/movies' do
-#   @title = params['title']
-#   url = "http://omdbapi.com/?t=#{@title}"
-#   movie_info = HTTParty.get url;
-#   @plot = movie_info["Plot"]
-#   @poster = movie_info["Poster"]
-#
-#   erb :movies #file in views folder
-# end
+get '/movielist' do
+  @id = params['id']
+  url = "http://omdbapi.com/?s=#{@id}"
+  movie_list = HTTParty.get url;
+
+end
+
+get '/movies' do
+  @title = params['title']
+  url = "http://omdbapi.com/?t=#{@title}"
+  movie_info = HTTParty.get url;
+  @plot = movie_info["Plot"]
+  @poster = movie_info["Poster"]
+
+  erb :movies #file in views folder
+end
